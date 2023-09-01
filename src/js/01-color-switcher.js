e9e05d157c9763fa418328cbea398224f3f9a213
OleksiiHaidak <xalexey.g@gmail.com>
2023-09-01 22:05:31 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBgColor = document.querySelector('body');
+let colorChangerStart = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+startBtn.addEventListener("click", () => {
+    if (!colorChangerStart) {
+colorChangerStart = setInterval(() => {
+      bodyBgColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+startBtn.classList.add('disabled');
+startBtn.disabled = true;
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(colorChangerStart);
+colorChangerStart = null;
+startBtn.classList.remove('disabled');
+startBtn.disabled = false;
+});
